6a25a7915d54d4f2d39efa748f2df15fcbad5371
Omkar Kaptan <omkarkaptan@gmail.com>
2017-03-15 15:14:07 -0700

    first iteration of metastore code

---
+++ headers/metastore.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+/* main meta store map
+    Key: Table name
+    Value: map containing metadata related to Table
+*/
+std::map<std::string, std::map> metastore;
