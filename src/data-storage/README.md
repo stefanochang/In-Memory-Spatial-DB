ea3416d880e9bf781636f1eede1a74f7333769f4
DhanashreeA <adhikari.dhanashree@gmail.com>
2017-03-19 22:32:17 -0700

    data store-first commit

---
+++ src/data-storage/README.md
@@ -0,0 +1,9 @@
+Currently we have added support for loading data items and all interfaces defined in the intergartion document.
+
+The APIs are defined in api.h
+The main table class is in list.h which defines the getnext method.
+
+Future scope:
+Support for sorted lists
+Concurrency
+Cache Consciousness
---
+++ src/data-storage/ds_api.h
@@ -0,0 +1,109 @@
+
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+#include "constants.h"
+#include "../integration/Geometry.h"
+
+using namespace std;
+
+//Initial data load supported for Points and Rectangles
+
+int loadData(string dbName, string tableName, int geomtype, string filepath, int collectionStruct)
+{
+	FILE *fp;
+	float x, y, x1, y1;
+	geometry *g;
+	point *pnt;
+	rectangle *rct;
+	list l;
+	fp = fopen(filepath.c_str(), "r");
+	if(fp == NULL)
+	{
+		return 0;
+	}
+	if(geomtype == TYPE_POINT)
+	{
+
+		while(fscanf(fp, "%f,%f\n", &x, &y) == 2)
+		{
+			g = (geometry *)malloc(sizeof(geometry));
+			pnt = (point *)malloc(sizeof(point));
+			pnt->x = x;
+			pnt->y = y;
+			g->pnt = pnt;
+			l.appendLast(g);
+		}
+	}
+	else if(geomtype == TYPE_RECTANGLE)
+	{
+		while(fscanf(fp, "%f,%f,%f,%f\n", &x, &y, &x1, &y1) == 4)
+		{
+			g = (geometry *)malloc(sizeof(geometry));
+			rct = (rectangle *)malloc(sizeof(rectangle));
+			rct->top_x = x;
+			rct->top_y = y;
+			rct->bottom_x = x1;
+			rct->bottom_y = y1;
+			g->rec = rct;
+			l.appendLast(g);
+		}
+	}
+	else
+	{
+		return 0;
+	}
+	// TODO: Call catalog functions to store table details
+
+	return 1;
+}
+
+// Insert a single point
+bool insertData(list pointsRepo, Point pointToInsert)
+{
+	geometry *g;
+	point *pnt;
+	g = (geometry *)malloc(sizeof(geometry));
+	pnt = (point *)malloc(sizeof(point));
+	g->pnt->x = pointToInsert.getCoordinates()[0];
+	g->pnt->y = pointToInsert.getCoordinates()[1];
+	pointsRepo.appendLast(g);
+	return true;
+}
+
+//insert a single rectangle
+bool insertData(list rectanglesRepo, Rectangle rectangleToInsert)
+{
+	geometry *g;
+	rectangle *rec;
+	g = (geometry *)malloc(sizeof(geometry));
+	rec = (rectangle *)malloc(sizeof(rectangle));
+	g->rec->top_x = rectangleToInsert.getCoordinates()[0];
+	g->rec->top_y = rectangleToInsert.getCoordinates()[1];
+	g->rec->bottom_x = rectangleToInsert.getCoordinates()[2];
+	g->rec->bottom_y = rectangleToInsert.getCoordinates()[3];
+	rectanglesRepo.appendLast(g);
+	return true;
+}
+
+//insert a list of items
+bool insertDataBulk(list repo, list geometryToInsert)
+{
+	record * geometryToInsertPointer = geometryToInsert.getHead();
+
+	while(geometryToInsertPointer != NULL) {
+		switch(repo.getType()) {
+			case TYPE_POINT: insertData(repo, *(geometryToInsertPointer->geom->pnt)); break;
+			case TYPE_RECTANGLE: insertData(repo, *(geometryToInsertPointer->geom->rec)); break;
+		}
+
+		geometryToInsertPointer = geometryToInsertPointer->next;
+	}
+	return true;
+}
+
+//delete item with id equals geomid
+bool deleteData(list repo, int geomId){
+	return repo.deleteByUUID(geomId);
+}
