a3fe3ab5af436fc146e8e110b68aa5fcc7efcc07
hariniravichandran <hariniravichandran1@gmail.com>
2017-03-30 22:55:40 -0700

    Removed constructors and added set methods to query tree

--- src/integration/query-tree.cpp
+++ src/integration/query-tree.cpp
@@ -1,97 +1,37 @@
 #include "query-tree.h"
 
-QueryTree::QueryTree(PointCollection leftPoints, vector<string> leftFilter) {
+QueryTree::setLeftPoints(PointCollection leftPoints) {
 	this->leftDataPoint = leftPoints;
-	this->leftFilter = leftFilter;
 }
 
-QueryTree::QueryTree(RectangleCollection leftRects, vector<string> leftFilter) {
+QueryTree::setLeftRectangles(RectangleCollection leftRects) {
 	this->leftDataRect = leftRects;
-	this->leftFilter = leftFilter;
 }
 
-QueryTree::QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter) {
+QueryTree::setLeftIndexedObject(SpatialIndexInterface leftIndexedObject) {
 	this->leftIndexedObject = leftIndexedObject;
+}
+
+QueryTree::setLeftFilter(vector<string> leftFilter) {
 	this->leftFilter = leftFilter;
 }
 
-QueryTree::QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root) {
-	this->leftDataPoint = leftPoints;
-	this->leftFilter = leftFilter;
+QueryTree::setRoot(vector<string> root) {
 	this->root = root;
 }
 
-QueryTree::QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root) {
-	this->leftDataRect = leftRects;
-	this->leftFilter = leftFilter;
-	this->root = root;
-}
-
-QueryTree::QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root) {
-	this->leftIndexedObject = leftIndexedObject;
-	this->leftFilter = leftFilter;
-	this->root = root;
-}
-
-QueryTree::QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root, 
-	PointCollection rightPoints,  vector<string> rightFilter) {
-	this->leftDataPoint = leftPoints;
-	this->leftFilter = leftFilter;
-	this->root = root;
+QueryTree::setRightPoints(PointCollection rightPoints) {
 	this->rightDataPoint = rightPoints;
-	this->rightFilter = rightFilter;
 }
 
-QueryTree::QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root, 
-	PointCollection rightPoints,  vector<string> rightFilter) {
-	this->leftDataRect = leftRects;
-	this->leftFilter = leftFilter;
-	this->root = root;
-	this->rightDataPoint = rightPoints;
-	this->rightFilter = rightFilter;
-}
-
-QueryTree::QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, 
-	PointCollection rightPoints,  vector<string> rightFilter) {
-	this->leftIndexedObject = leftIndexedObject;
-	this->leftFilter = leftFilter;
-	this->root = root;
-	this->rightDataPoint = rightPoints;
-	this->rightFilter = rightFilter;
-}
-
-QueryTree::QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root, 
-	RectangleCollection rightRects,  vector<string> rightFilter) {
-	this->leftDataPoint = leftPoints;
-	this->leftFilter = leftFilter;
-	this->root = root;
+QueryTree::setRightRectangles(RectangleCollection rightRects) {
 	this->rightDataRect = rightRects;
-	this->rightFilter = rightFilter;
 }
 
-QueryTree::QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root, 
-	RectangleCollection rightRects,  vector<string> rightFilter) {
-	this->leftDataRect = leftRects;
-	this->leftFilter = leftFilter;
-	this->root = root;
-	this->rightDataRect = rightRects;
-	this->rightFilter = rightFilter;
-}
-
-QueryTree::QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, 
-	RectangleCollection rightRects,  vector<string> rightFilter) {
-	this->leftIndexedObject = leftIndexedObject;
-	this->leftFilter = leftFilter;
-	this->root = root;
-	this->rightDataRect = rightRects;
-	this->rightFilter = rightFilter;
-}
-
-QueryTree::QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, 
-	SpatialIndexInterface rightIndexedObject,  vector<string> rightFilter) {
-	this->leftIndexedObject = leftIndexedObject;
-	this->leftFilter = leftFilter;
-	this->root = root;
+QueryTree::setRightIndexedObject(SpatialIndexInterface rightIndexedObject) {
 	this->rightIndexedObject = rightIndexedObject;
+}
+
+QueryTree::setRightFilter(vector<string> rightFilter) {
 	this->rightFilter = rightFilter;
 }
--- src/integration/query-tree.h
+++ src/integration/query-tree.h
@@ -13,13 +13,13 @@ class QueryTree {
 
 public:
     QueryTree();
-    void set_root(vector<string>);
-    void set_left_filter(vector<string>);
-    void set_left_points(PointCollection);
-    void set_left_rectangles(RectangleCollection);
-    void set_left_indexed_object(SpatialIndexInterface);
-    void set_right_filter(vector<string>);
-    void set_right_points(PointCollection);
-    void set_right_rectangles(RectangleCollection);
-    void set_right_indexed_object(SpatialIndexInterface);
+    void setRoot(vector<string>);
+    void setLeftFilter(vector<string>);
+    void setLeftPoints(PointCollection);
+    void setLeftRectangles(RectangleCollection);
+    void setLeftIndexedObject(SpatialIndexInterface);
+    void setRightFilter(vector<string>);
+    void setRightPoints(PointCollection);
+    void setRightRectangles(RectangleCollection);
+    void setRightIndexedObject(SpatialIndexInterface);
 };
