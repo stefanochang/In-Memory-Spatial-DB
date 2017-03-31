e8cad238031b5b49f275548bb1e86bf9461fddb5
Manasa <manasa.venkatakrishnan@gmail.com>
2017-03-30 21:09:10 -0700

    Added constructors to Query Tree

--- src/integration/query-tree.h
+++ src/integration/query-tree.h
@@ -1,4 +1,4 @@
-#include "geometry-collection.h"
+#include "spatial-index-interface.h"
 
 class QueryTree {
     vector<string> root;
@@ -8,4 +8,21 @@ class QueryTree {
     vector<string> rightFilter;
     PointCollection rightDataPoint;
     RectangleCollection rightDataRect;
+    SpatialIndexInterface rightIndexedObject;
+	SpatialIndexInterface leftIndexedObject;
+
+public:
+    QueryTree(PointCollection leftPoints, vector<string> leftFilter);
+    QueryTree(RectangleCollection leftRects, vector<string> leftFilter);
+    QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter);
+    QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root);
+    QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root);    
+    QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root);
+    QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root, PointCollection rightPoints,  vector<string> rightFilter);
+    QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root, PointCollection rightPoints,  vector<string> rightFilter);
+    QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, PointCollection rightPoints,  vector<string> rightFilter);
+    QueryTree(PointCollection leftPoints, vector<string> leftFilter, vector<string> root, RectangleCollection rightRects,  vector<string> rightFilter);
+    QueryTree(RectangleCollection leftRects, vector<string> leftFilter, vector<string> root, RectangleCollection rightRects,  vector<string> rightFilter);
+    QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, RectangleCollection rightRects,  vector<string> rightFilter);
+    QueryTree(SpatialIndexInterface leftIndexedObject, vector<string> leftFilter, vector<string> root, SpatialIndexInterface rightIndexedObject,  vector<string> rightFilter);
 };
