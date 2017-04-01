268c93ae48aa93abd2e220202558a8a67fc94f3d
hariniravichandran <hariniravichandran1@gmail.com>
2017-03-31 19:02:49 -0700

    Update geometry-collection.h (added new functions)

--- src/integration/geometry-collection.h
+++ src/integration/geometry-collection.h
@@ -12,8 +12,13 @@ class PointCollection {
     Point getById(int);
     vector<Point> getNext(int n=1);
     int insert(Point);
+    int insertBulk(PointCollection);
+    bool isEmpty();
     int remove(Point);
     int removeById(int);
+    int getSize();  // size of PointCollection
+    string getDBName();
+    string getTableName();
 };
 
 class RectangleCollection {
@@ -26,8 +31,13 @@ class RectangleCollection {
     Rectangle getById(int);
     vector<Rectangle> getNext(int n=1);
     int insert(Rectangle);
+    int insertBulk(RectangleCollection);
     int remove(Rectangle);
     int removeById(int);
+    bool isEmpty();
+    int getSize();
+    string getDBName();
+    string getTableName();
 };
 
 class PointPointCollection {
@@ -39,6 +49,12 @@ class PointPointCollection {
     PointPointCollection(string, string, int, vector<PointPoint>);
     PointPoint getById(int);
     vector<PointPoint> getNext(int n=1);
+    int insert(PointPoint);
+    int insertBulk(PointPointCollection);
+    bool isEmpty();
+    int getSize();
+    string getDBName();
+    string getTableName();
 };
 
 class PointRectangleCollection {
@@ -50,6 +66,12 @@ class PointRectangleCollection {
     PointRectangleCollection(string, string, int, vector<PointRectangle>);
     PointRectangle getById(int);
     vector<PointRectangle> getNext(int n=1);
+    int insert(PointRectangle);
+    int insertBulk(PointRectangleCollection);
+    bool isEmpty();
+    int getSize();
+    string getDBName();
+    string getTableName();
 };
 
 class RectangleRectangleCollection {
@@ -61,4 +83,10 @@ class RectangleRectangleCollection {
     RectangleRectangleCollection(string, string, int, vector<RectangleRectangle>);
     RectangleRectangle getById(int);
     vector<RectangleRectangle> getNext(int n=1);
+    int insert(RectangleRectangle);
+    int insertBulk(RectangleRectangleCollection);
+    bool isEmpty();
+    int getSize();
+    string getDBName();
+    string getTableName();
 };
