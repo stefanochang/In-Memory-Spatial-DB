5316bad91c3cfb8d6fc19ed27c39bdaf5a68d839
hariniravichandran <hariniravichandran1@gmail.com>
2017-03-31 18:52:59 -0700

    Update catalog to latest version (reversed commit)
    Commit was reversed by mistake. Re-committing it.

--- src/integration/catalog.h
+++ src/integration/catalog.h
@@ -2,49 +2,57 @@
 #include <map>
 class CatalogItem {
   string dbName, tableName;
-  PointCollection points;
-  RectangleCollection rectangles;
-  PointPointCollection pointsPoints;
-  RectangleRectangleCollection rectanglesRectangles;
-  PointRectangleCollection pointsRectangles;
-  SpatialIndexInterface spatialIndexObject; //Handle for spatial partitioned indexed object
-  SpatialIndexInterface dataIndexObject;  //Handle for data Indexed object
+  int collectionType;
+  PointCollection* points;
+  RectangleCollection* rectangles;
+  PointPointCollection* pointsPoints;
+  RectangleRectangleCollection* rectanglesRectangles;
+  PointRectangleCollection* pointsRectangles;
+  SpatialIndexInterface* spatialIndexObject; //Handle for spatial partitioned indexed object
+  SpatialIndexInterface* dataIndexObject;  //Handle for data Indexed object
   map<string, float> statistics;
 protected:
   void setStatistic(string, float);
 public:
-  CatalogItem(string, string);
-  CatalogItem(string, string, PointCollection);
-  CatalogItem(string, string, RectangleCollection);
-  CatalogItem(string, string, PointPointCollection);
-  CatalogItem(string, string, RectangleRectangleCollection);
-  CatalogItem(string, string, PointRectangleCollection);
+  CatalogItem(string, string, int);
+  CatalogItem(string, string, int, PointCollection*);
+  CatalogItem(string, string, int, RectangleCollection*);
+  CatalogItem(string, string, int, PointPointCollection*);
+  CatalogItem(string, string, int, RectangleRectangleCollection*);
+  CatalogItem(string, string, int, PointRectangleCollection*);
+  string getDBName();
+  string getTableName();
   float getStatistic(string);
   bool hasSpatialIndex();
   bool hasDataIndex();
-  void addSpatialIndex(SpatialIndexInterface);
-  void addDataIndex(SpatialIndexInterface);
-  PointCollection getPointCollection();
-  RectangleCollection getRectangleCollection();
-  PointPointCollection getPointPointCollection();
-  PointRectangleCollection getPointRectangleCollection();
-  RectangleRectangleCollection getRectangleRectangleCollection();
-  vector<SpatialIndexInterface> getIndexedObjects();  
+  SpatialIndexInterface* getSpatialIndex();
+  SpatialIndexInterface* getDataIndex();
+  void addSpatialIndex(SpatialIndexInterface*);
+  void addDataIndex(SpatialIndexInterface*);
+  PointCollection* getPointCollection();
+  RectangleCollection* getRectangleCollection();
+  PointPointCollection* getPointPointCollection();
+  PointRectangleCollection* getPointRectangleCollection();
+  RectangleRectangleCollection* getRectangleRectangleCollection();
+  vector<SpatialIndexInterface*> getIndexObjects();  
 };
 
 //To be defined as a Singleton Class.
 class Catalog {
-  CatalogItem* catalogList;
+  vector<CatalogItem*> catalogList;
 protected:
-  int remove(CatalogItem);
-  int insert(CatalogItem);
+  int remove(string, string);
+  int insert(CatalogItem*);
 public:
+  CatalogItem* getCatalogItem(string, string);
+  SpatialIndexInterface* getSpatialIndexedCollection(string, string);
+  SpatialIndexInterface* getDataIndexedCollection(string, string);
+  /*
   vector<SpatialIndexInterface> getSpatialIndexedPointCollections();
   vector<SpatialIndexInterface> getDataIndexedPointCollections();
   vector<SpatialIndexInterface> getSpatialIndexedRectangleCollections();
   vector<SpatialIndexInterface> getDataIndexedRectangleCollections();
-  PointCollection getPointCollectionsWithoutIndex();
-  RectangleCollection getRectangleCollectionsWithoutIndex();
-  PointCollection getPointCollectionByName(string);
-  RectangleCollection getRectangleCollectionByName(string);
+  */
+  PointCollection* getPointCollectionByName(string, string);
+  RectangleCollection* getRectangleCollectionByName(string, string);
 };
