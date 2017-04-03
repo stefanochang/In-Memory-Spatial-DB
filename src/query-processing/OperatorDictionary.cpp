cff51b05c98bd055fdaf840a82f27db2b2d26504
Nikhil Vementala <nvementa@asu.edu>
2017-04-03 09:39:43 -0700

    added

--- src/query-processing/OperatorDictionary.cpp
+++ src/query-processing/OperatorDictionary.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
 #include "../integration/query-processing.h"
 
 using namespace std;
@@ -19,12 +21,10 @@ bool OperatorDictionary::applyFilterBy (vector<string> filterDetails, Point inpu
 	if (filterDetails[1] == "distance") {
 		double metricVal;
 		if (filterDetails.size()==8) {
-			metricVal = PointOperations::getDistance(inputPoint,
-				Rectangle(stod(filterDetails[4]),stod(filterDetails[5]),stod(filterDetails[6]),stod(filterDetails[7])));
+			metricVal = PointOperations::getDistance(inputPoint,Rectangle(stod(filterDetails[4]),stod(filterDetails[5]),stod(filterDetails[6]),stod(filterDetails[7])));
 		}
 		else {
-			metricVal = PointOperations::getDistance(inputPoint,
-				Point(stod(filterDetails[4]),stod(filterDetails[5])));
+			metricVal = PointOperations::getDistance(inputPoint,Point(stod(filterDetails[4]),stod(filterDetails[5])));
 		}
 		if(operatorName == "gt" && metricVal > compareVal)
 			isCriterionSatisfied = true;
@@ -48,8 +48,8 @@ bool OperatorDictionary::applyFilterBy (vector<string> filterDetails, Rectangle 
 	if (filterDetails[1] == "distance") {
 		double metricVal;
 		if (filterDetails.size()==8) {
-			metricVal = RectangleOperations::getDistance(inputRect,
-				Rectangle(stod(filterDetails[4]),stod(filterDetails[5]),stod(filterDetails[6]),stod(filterDetails[7])));
+			metricVal = RectangleOperations::getDistance(inputRect,Rectangle(stod(filterDetails[4]),stod(filterDetails[5]),stod(filterDetails[6]),stod(filterDetails[7])));
+			//cout << metricVal << " - ";
 		}
 		else {
 			metricVal = PointOperations::getDistance(Point(stod(filterDetails[4]),stod(filterDetails[5])), inputRect);
