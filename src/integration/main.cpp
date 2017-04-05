7163ede5e09be56fb375f7cb683cefe8fe28a0ac
Manasa <manasa.venkatakrishnan@gmail.com>
2017-04-04 19:51:08 -0700

    QueryTree building for multilevel filters

--- src/integration/main.cpp
+++ src/integration/main.cpp
@@ -71,17 +71,36 @@ int main() {
             int left_param_end = query_param.find("]"); 
             string left_branch = query_param.substr(1, left_param_end-1);            
             
-            // QueryTree query = new QueryTree();
-            vector<string> collection_details = split(left_branch.substr(0, left_branch.find("|")), " ");
-            if(collection_details[0] == "POINT") {
-                // query.setLeftPoints(catalog.getPointCollectionByName(collection_details[1], collection_details[2]));
+            QueryTree query = new QueryTree();
+            vector<string> left_collection_details = split(left_branch.substr(0, left_branch.find("|")), " ");
+            if(left_collection_details[0].compare("POINT") == 0) {
+                // query.setLeftPoints(catalog.getPointCollectionByName(left_collection_details[1], left_collection_details[2]));
             } else {
-                // query.setLeftRectangles(catalog.getRectangleCollectionByName(collection_details[1], collection_details[2]));
+                // query.setLeftRectangles(catalog.getRectangleCollectionByName(left_collection_details[1], left_collection_details[2]));
             }
             vector<vector<string> > left_filter_param = get_predicates_from_string(left_branch);
-            // query.setLeftFilter(left_filter_param);
+            query.setLeftFilter(left_filter_param);
+            int root_param_end = query_param.substr(left_param_end+3).find("]");            
+            string root = query_param.substr(left_param_end+3, root_param_end);
+            vector<string> root_filter_param = split(root, ":");
+            query.setRoot(root_filter_param);
 
+            int right_param_end = query_param.substr(left_param_end+root_param_end+6).find("]"); 
+            string right_branch = query_param.substr(left_param_end+root_param_end+6, right_param_end);            
+            
+            vector<string> right_collection_details = split(right_branch.substr(0, right_branch.find("|")), " ");
+            if(right_collection_details[0].compare("POINT") == 0) {
+                // query.setRightPoints(catalog.getPointCollectionByName(right_collection_details[1], right_collection_details[2]));
+            } else {
+                // query.setRightRectangles(catalog.getRectangleCollectionByName(right_collection_details[1], right_collection_details[2]));
+            }
+            vector<vector<string> > right_filter_param = get_predicates_from_string(right_branch);
+            query.setRightFilter(right_filter_param);
+            // QueryResult result = processQuery(query);
+                        
 
+        } else if(query_tokens[0].compare("EXIT") == 0) {
+            exit(0);
         } else {
             cout << "Invalid command\n";        
         }
