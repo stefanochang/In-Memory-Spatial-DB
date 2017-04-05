625eb1e3d6f27f90538147cd1f2e37760dfef71b
unknown <okaptan@EN4110017214B.fulton.ad.asu.edu>
2017-04-04 20:39:11 -0700

    added test for getNext

--- src/data-storage/main.cpp
+++ src/data-storage/main.cpp
@@ -5,16 +5,52 @@
 #include "data-storage.h"
 
 using namespace std;
-int callLoadData();
-int main()
-{
-    int status;
-    status = callLoadData();
-    cout<<"Returned status:"<<status<<endl;
-    return 0;
+
+bool testGetNextSingle() {
+  PointCollection *pc = new PointCollection();
+
+  Point *testPoint = new Point(1, 2);
+
+  pc->insert(*testPoint);
+
+  vector<Point> result = pc->getNext(1, 1);
+  bool testResultSize = result.size() == 1;
+
+  vector<float> resultPointCoordinates = result[0].getCoordinates();
+  bool testResultPointCoordinates = resultPointCoordinates[0] == 1 && resultPointCoordinates[1] == 2;
+
+  return testResultSize; // && testResultPointCoordinates;
 }
 
-int callLoadData()
+bool testGetNextMultiple() {
+  PointCollection *pc = new PointCollection();
+
+  Point *testPoint = new Point(1, 2);
+  Point *testPoint2 = new Point(3, 4);
+
+  pc->insert(*testPoint);
+  pc->insert(*testPoint2);
+
+  vector<Point> result = pc->getNext(2, 1);
+  bool testResultSize = result.size() == 2;
+
+  vector<float> resultPointCoordinates = result[0].getCoordinates();
+  bool testResultPointCoordinates = resultPointCoordinates[0] == 1 && resultPointCoordinates[1] == 2;
+
+  vector<float> resultPointCoordinates2 = result[1].getCoordinates();
+  bool testResultPointCoordinates2 = resultPointCoordinates2[0] == 3 && resultPointCoordinates2[1] == 4;
+
+  return testResultSize && testResultPointCoordinates && testResultPointCoordinates2;
+}
+
+bool testGetNext() {
+  return testGetNextSingle() && testGetNextMultiple();
+}
+
+int main()
 {
-  return loadData("data-storage","pointstable",TYPE_POINT, "pointsdata", 1);
+    //loadData("mdd","ddd",1,"ddd",2);
+    //return 0;
+
+    cout << "Result for getNext: " << testGetNext();
 }
