d5cb22becd8df40f1d6210a3de48c4b548775fe7
DhanashreeA <adhikari.dhanashree@gmail.com>
2017-04-04 17:21:11 -0700

    changed structure for all APIs

---
+++ src/data-storage/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "data-storage.h"
+
+using namespace std;
+
+int main()
+{
+    loadData("mdd","ddd",1,"ddd",2);
+    return 0;
+}
