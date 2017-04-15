a433000c3381bf94885058d49531d323018b2390
supriya <sashokk2@asu.edu>
2017-04-15 10:31:50 -0700

    Edited the testcase and added new

--- src/query-processing/QueryProcessingTest.cpp
+++ src/query-processing/QueryProcessingTest.cpp
@@ -18,42 +18,403 @@
 #define TYPE_POINTRECTANGLE 4
 #define TYPE_RECTANGLERECTANGLE 5
 
+#define NO_JOIN '1'
+#define RANGE_JOIN '2'
+#define KNN_JOIN '3'
+#define DISTANCE_JOIN '4'
+
+#define FILTER_BY_AREA_LT 'a'
+#define FILTER_BY_AREA_LE 'b'
+#define FILTER_BY_AREA_EQ 'c'
+#define FILTER_BY_AREA_GT 'd'
+#define FILTER_BY_AREA_GE 'e'
+#define NO_FILTER ''
+
+#define FILTER_BY_DISTANCE_LT 'f'
+#define FILTER_BY_DISTANCE_LE 'g'
+#define FILTER_BY_DISTANCE_EQ 'h'
+#define FILTER_BY_DISTANCE_GT 'i'
+#define FILTER_BY_DISTANCE_GE 'j'
+
+#define KNN 11
+#define OBJECTS_IN_RANGE 12
+
 using namespace std;
 
 
 bool queryProcessingTests() {
 
-	QueryTree qTree1,qTree2,qTree3,qTree4,qTree5,qTree6,qTree7;
-	QueryResult qResult1,qResult2,qResult3,qResult4,qResult5,qResult6,qResult7;
-	QueryProcessing query1,query2,query3,query4,query5,query6,query7;
 
-	vector<string> rootnull = { "" };
-	vector<vector<string> > leftfilternull;
-	leftfilternull.push_back(rootnull);
-	vector<vector<string> > rightfilternull;
-	rightfilternull.push_back(rootnull);
+    vector<Filter> filterNull;
+
 	PointCollection ptcolNull;
 	RectangleCollection retcolNull;
 
 
-	//The query tree root and left and right all is null
-	//Test 1
-	qTree1.setRoot(rootnull);
-	qTree1.setLeftFilter(leftfilternull);
-	qTree1.setLeftPoints(ptcolNull);
-	qTree1.setLeftRectangles(retcolNull);
-	//qTree1.setLeftIndexedObject(null);
-	qTree1.setRightFilter(rightfilternull);
-	qTree1.setRightPoints(ptcolNull);
-	qTree1.setRightRectangles(retcolNull);
-	//qTree1.setRightIndexedObject(null));
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 1 : FilterBy Distance -\n\t Find the points that are greater than distance 2 from point (0,0)";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(NO_JOIN);
 
-	qResult1 = query1.processQuery(qTree1);
+		vector<float> inputParams;
+		inputParams.push_back(2);
+		inputParams.push_back(0);
+		inputParams.push_back(0);
 
-	cout<<"\n-----------------Test 1------------------\n";
-	if (qResult1.getResultType() == TYPE_NULL)
-		cout<< "Pass Test 1"<<endl;
-    //---------------------------------------------------------------
+		char ft=FILTER_BY_DISTANCE_GT;
+		Filter f = Filter(ft,inputParams);
+		vector<Filter> filter;
+		filter.push_back(f);
+		qTree.setLeftFilter(filter);
+
+		qTree.setRightFilter(filterNull);
+
+		PointCollection ptcol;
+
+		cout<<"POINT DATASET\n";
+		for(int i=0;i<5;i++){
+			cout<<"(0,"<<i<<")\n";
+			ptcol.insert(Point(0,i));
+		}
+		qTree.setLeftPoints(ptcol);
+
+		qTree.setLeftRectangles(retcolNull);
+		qTree.setRightPoints(ptcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+		if (qResult.getResultType() == TYPE_POINT){
+			PointCollection ppcol = qResult.getPointCollection();
+			cout<<"\nRESULT: PointCollection"<<endl;
+			for(int i=0;i<ppcol.getSize();i++)
+			{
+				vector<Point> temp_ptv = ppcol.getNext(1);
+				for (Point pt : temp_ptv)
+					cout<< "( "<< pt.getCoordinates().at(0)<< " ," << pt.getCoordinates().at(1) << " )" <<endl;
+			}
+		}
+		else{
+			cout << "Fail Test 1"<<endl;
+		}
+	}
+
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 2 : FilterBy Distance -\n\t Find the points that are equal to distance 5 from point (0,1)";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(NO_JOIN);
+
+		vector<float> inputParams;
+		inputParams.push_back(5);
+		inputParams.push_back(0);
+		inputParams.push_back(1);
+
+		char ft=FILTER_BY_DISTANCE_EQ;
+		Filter f = Filter(ft,inputParams);
+		vector<Filter> filter;
+		filter.push_back(f);
+		qTree.setLeftFilter(filter);
+
+		qTree.setRightFilter(filterNull);
+
+		PointCollection ptcol;
+
+		cout<<"POINT DATASET\n";
+		for(int i=4;i<8;i++){
+			cout<<"(0,"<<i<<")\n";
+			ptcol.insert(Point(0,i));
+		}
+		qTree.setLeftPoints(ptcol);
+
+		qTree.setLeftRectangles(retcolNull);
+		qTree.setRightPoints(ptcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+		if (qResult.getResultType() == TYPE_POINT){
+			PointCollection ppcol = qResult.getPointCollection();
+			cout<<"\nRESULT: PointCollection"<<endl;
+			for(int i=0;i<ppcol.getSize();i++)
+			{
+				vector<Point> temp_ptv = ppcol.getNext(1);
+				for (Point pt : temp_ptv)
+					cout<< "( "<< pt.getCoordinates().at(0)<< " ," << pt.getCoordinates().at(1) << " )" <<endl;
+			}
+		}
+		else{
+			cout << "Fail Test 2"<<endl;
+		}
+	}
+
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 3 : FilterBy Distance -\n\t Find the Rectangles that are less than distance 2 from Rectangle (0,0,1,1)";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(NO_JOIN);
+
+		vector<float> inputParams;
+		inputParams.push_back(2);
+		inputParams.push_back(0);
+		inputParams.push_back(0);
+		inputParams.push_back(1);
+		inputParams.push_back(1);
+
+		char ft=FILTER_BY_DISTANCE_LT;
+		Filter f = Filter(ft,inputParams);
+		vector<Filter> filter;
+		filter.push_back(f);
+		qTree.setLeftFilter(filter);
+
+		qTree.setRightFilter(filterNull);
+
+		RectangleCollection retcol;
+
+		cout<<"RECTANGLE DATASET\n";
+		for(int i=0;i<6;i++){
+			cout<<"("<<i<<","<<i<<"), ("<<i+2<<","<<i+2<<")\n";
+			retcol.insert(Rectangle(i,i,i+2,i+2));
+		}
+		qTree.setLeftRectangles(retcol);
+
+		qTree.setLeftPoints(ptcolNull);
+		qTree.setRightPoints(ptcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+		if (qResult.getResultType() == TYPE_RECTANGLE){
+			RectangleCollection rcoll = qResult.getRectangleCollection();
+			cout<<"\nRESULT: RectangeCollection"<<endl;
+			for(int i=0;i<rcoll.getSize();i++)
+			{
+				vector<Rectangle> temp_ptv = rcoll.getNext(1);
+				for (Rectangle rt : temp_ptv){
+					cout<< "(" << rt.getCoordinates().at(0)<< " ," << rt.getCoordinates().at(1)<< "), ("
+						<< rt.getCoordinates().at(2)<< " ," << rt.getCoordinates().at(3)<<")"<<endl;
+				}
+			}
+		}
+		else{
+			cout << "Fail Test 3"<<endl;
+		}
+
+	}
+
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 4 : Two FilterBy Distance on same collection -\n\t Find the Rectangles that are greater than distance 2 and\n\t less than distance from Rectangle (0,0,1,1)";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(NO_JOIN);
+
+		vector<float> inputParams1;
+		inputParams1.push_back(2);
+		inputParams1.push_back(0);
+		inputParams1.push_back(0);
+		inputParams1.push_back(1);
+		inputParams1.push_back(1);
+
+		char ft1=FILTER_BY_DISTANCE_GT;
+		Filter f1 = Filter(ft1,inputParams1);
+
+		vector<float> inputParams2;
+		inputParams2.push_back(4);
+		inputParams2.push_back(0);
+		inputParams2.push_back(0);
+		inputParams2.push_back(1);
+		inputParams2.push_back(1);
+
+		char ft2=FILTER_BY_DISTANCE_LT;
+		Filter f2 = Filter(ft2,inputParams2);
+
+		vector<Filter> filter;
+		filter.push_back(f1);
+		filter.push_back(f2);
+		qTree.setLeftFilter(filter);
+
+		qTree.setRightFilter(filterNull);
+
+		RectangleCollection retcol;
+
+		cout<<"RECTANGLE DATASET\n";
+		for(int i=0;i<5;i++){
+			cout<<"("<<i<<","<<i<<"), ("<<i+2<<","<<i+2<<")\n";
+			retcol.insert(Rectangle(i,i,i+2,i+2));
+		}
+		qTree.setLeftRectangles(retcol);
+
+		qTree.setLeftPoints(ptcolNull);
+		qTree.setRightPoints(ptcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+		if (qResult.getResultType() == TYPE_RECTANGLE){
+			RectangleCollection rcoll = qResult.getRectangleCollection();
+			cout<<"\nRESULT: RectangeCollection"<<endl;
+			for(int i=0;i<rcoll.getSize();i++)
+			{
+				vector<Rectangle> temp_ptv = rcoll.getNext(1);
+				for (Rectangle rt : temp_ptv){
+					cout<< "(" << rt.getCoordinates().at(0)<< " ," << rt.getCoordinates().at(1)<< "), ("
+						<< rt.getCoordinates().at(2)<< " ," << rt.getCoordinates().at(3)<<")"<<endl;
+				}
+			}
+		}
+		else{
+			cout << "Fail Test 4"<<endl;
+		}
+
+	}
+
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 5 : FilterBy Area\n\t Find all the Rectangles that have area greater than 5";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(NO_JOIN);
+
+		vector<float> inputParams;
+		inputParams.push_back(5);
+
+		char ft=FILTER_BY_DISTANCE_GT;
+		Filter f = Filter(ft,inputParams);
+
+		vector<Filter> filter;
+		filter.push_back(f);
+
+		qTree.setLeftFilter(filter);
+
+		qTree.setRightFilter(filterNull);
+
+		RectangleCollection retcol;
+
+		cout<<"RECTANGLE DATASET\n";
+		for(int i=1;i<7;i++){
+			cout<<"("<<i<<","<<i<<"), ("<<i+i<<","<<i+i<<")\n";
+			retcol.insert(Rectangle(i,i,i+i,i+i));
+		}
+		qTree.setLeftRectangles(retcol);
+
+		qTree.setLeftPoints(ptcolNull);
+		qTree.setRightPoints(ptcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+
+
+		if (qResult.getResultType() == TYPE_RECTANGLE){
+			RectangleCollection rcoll = qResult.getRectangleCollection();
+			cout<<"\nRESULT: RectangeCollection"<<endl;
+			for(int i=0;i<rcoll.getSize();i++)
+			{
+				vector<Rectangle> temp_ptv = rcoll.getNext(1);
+				for (Rectangle rt : temp_ptv){
+					cout<< "(" << rt.getCoordinates().at(0)<< " ," << rt.getCoordinates().at(1)<< "), ("
+						<< rt.getCoordinates().at(2)<< " ," << rt.getCoordinates().at(3)<<")"<<endl;
+				}
+			}
+		}
+		else{
+			cout << "Fail Test 5"<<endl;
+		}
+
+	}
+
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	cout<<"Test 6 : Distance Join\n";
+	cout<<"\t Filterby left branch all points whose distance is greater than 3 from point (0,0)\n";
+    cout<<"\t Filterby right branch all points whose distance is less than 5 from point (0,3)\n";
+	cout<<"\t DistanceJoin all points whose distance is less than 5";
+	cout<<"\n---------------------------------------------------------------------------------------------\n";
+	{
+		QueryTree qTree;
+		QueryResult qResult;
+		QueryProcessing query;
+		qTree.setRootType(DISTANCE_JOIN);
+		qTree.setRootParam(5);
+
+		vector<float> inputParamsleft;
+		inputParamsleft.push_back(3);
+		inputParamsleft.push_back(0);
+		inputParamsleft.push_back(0);
+
+		char fl=FILTER_BY_DISTANCE_GT;
+		Filter f_left = Filter(fl,inputParamsleft);
+
+		vector<Filter> filter_left;
+		filter_left.push_back(f_left);
+
+		qTree.setLeftFilter(filter_left);
+
+		vector<float> inputParamsright;
+		inputParamsright.push_back(5);
+		inputParamsright.push_back(0);
+		inputParamsright.push_back(3);
+
+		char fr=FILTER_BY_DISTANCE_LT;
+		Filter f_right = Filter(fr,inputParamsright);
+
+		vector<Filter> filter_right;
+		filter_right.push_back(f_right);
+
+		qTree.setRightFilter(filter_right);
+
+		cout<<"POINT DATASET LEFT\t\tPOINT DATASET RIGHT";
+		PointCollection ptcolleft,ptcollright;
+
+		for(int i=0;i<5;i++){
+			ptcolleft.insert(Point(0,i));
+			ptcollright.insert(Point(0,i+2));
+			cout<<"\n\t("<<0<<","<<i<<")\t\t\t\t("<<0<<","<<i+2<<")";
+		}
+
+		qTree.setLeftPoints(ptcolleft);
+		qTree.setRightPoints(ptcollright);
+
+		qTree.setLeftRectangles(retcolNull);
+		qTree.setRightRectangles(retcolNull);
+
+		qResult = query.processQuery(qTree);
+
+		if (qResult.getResultType() == TYPE_POINTPOINT){
+			PointPointCollection ptcoll = qResult.getPointPointCollection();
+			cout<<"\nRESULT: PointPointCollection"<<endl;
+			cout << "ResultSet Size : " << ptcoll.getSize() << "\n";
+			for(int i=0;i<ptcoll.getSize();i++)
+			{
+				vector<PointPoint> tempt_ppt = ptcoll.getNext(1);
+				for ( PointPoint ppt : tempt_ppt){
+					cout<< "[(" << ppt.getCoordinates().at(0)<< " ," << ppt.getCoordinates().at(1) <<"),"
+						<< "("<< ppt.getCoordinates().at(2)<< " ," << ppt.getCoordinates().at(3) <<") ]"<<endl;
+				}
+			}
+		}
+		else{
+			cout << "Fail Test 6"<<endl;
+		}
+
+	}
+
+   //---------------------------------------------------------------
+	/*
 	//Test 2
 	//The return all the points whose distance is less than 5 from Point(0,0)
 	qTree2.setRoot(rootnull);
@@ -388,5 +749,6 @@ bool queryProcessingTests() {
 	else{
 		cout << "Fail Test 7"<<endl;
 	}
+	*/
 }
 
